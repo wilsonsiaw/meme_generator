6a56c525fd43a6a70c372dafa245d080129775ac
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-31 16:23:31 +0200

    ✨ feat(Header component): Create header
    Add the content for the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../../public/images/logo.svg'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='header'>
+      <img src={logo} alt="A logo for the application" />
+      <h1>MemeGenerator</h1>
     </div>
   )
 }
