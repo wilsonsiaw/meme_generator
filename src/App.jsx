f8947f4985103761d67aa09106aaf30d18fad5e6
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-31 16:20:37 +0200

    ✨ feat(Application): Add header
    Add the Header component to the root component (Application)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
