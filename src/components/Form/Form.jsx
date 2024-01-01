f666196dbaeb53bee5f7232fc7ecdb441ca36059
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-01 22:22:08 +0200

    ✨ feat(Form component): Add Meme Image
    Add a function that changes the state of the application and renders an image

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Form.css'
+import memesData from '../../memesData'
 
 const Form = () => {
+
+    // create state
+    const [memeImage, setMemeImage] = useState("");
+
+    // create a function that will randomly get a meme image
+    // and render that meme image
+    const getMemeImage = () => {
+      const memesArray = memesData.data.memes;
+      const randomNumber = Math.floor(Math.random() * memesArray.length)
+      const url = memesArray[randomNumber].url;
+      setMemeImage(url);
+    }
+
+
   return (
     <div>
-      <form action="" className='formWrapper'>
+      <div className='formWrapper'>
         <div className="formGroup">
             <label htmlFor="topText" className='top'>Top text</label>
             <input type="text" id='topText' name='topText'/>
@@ -13,9 +28,9 @@ const Form = () => {
             <label htmlFor="bottomText" className='bottom'>Bottom text</label>
             <input type="text" id='bottomText' name='bottomText'/>
         </div>
-        <button>Get a new meme image  🖼</button>
-      </form>
-      <div className='meme'></div>
+        <button onClick={getMemeImage}>Get a new meme image  🖼</button>
+        <img src={memeImage} className='meme-image'/>
+      </div>
     </div>
   )
 }
